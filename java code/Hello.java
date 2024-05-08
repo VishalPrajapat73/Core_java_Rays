1a9621674e056e8f87d56127fd215912cc2588ef
VishalPrajapat73 <vishalprajapat7354@gmail.com>
2024-05-08 19:08:47 +0530

    Code

---
+++ java code/Hello.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	// 	System.out.println("\n* * * * * * * * * * * * * * * * * * * * * * * ");
+	// 	System.out.println("1. Add \t 2. sub \t 3. mul \t 4. div ");
+	// 	System.out.println("Enter your choice : ");
+	// 	int c=sc.nextInt();
+	// 	if(c>0&&c<=4){
+	// 	System.out.println("Enter value of a and b  : ");
+	// 	int a = sc.nextInt();
+	// 	int b = sc.nextInt();
+	// 	switch (c) {
+	// 		case 1 :
+	// 			System.out.println("Sum of a+b is : " +(a+b));
+	// 			break;
+	// 		case 2:
+	// 			System.out.println("Sub of a-b is : "+(a-b));
+	// 			break;
+	// 		case 3:
+	// 			System.out.println("Mul of a*b is : "+(a*b));
+	// 			break;
+	// 		case 4:
+	// 			System.out.println("Div of a/b is : "+(a/b));
+	// 			break;
+	// 		default:
+	// 			System.out.println("!!!!! Invalid Choice !!!!!!");
+	// 			break;
+	// 	}
+	// }else {
+	// 	System.out.println("Invalid Choice ");
+	// }
+
+		}
+
+}
---
+++ java code/Radha.java
@@ -0,0 +1,20 @@
+public class Radha{
+public static void main (String[]args){
+
+int i=99;
+
+if(i>100){
+	System.out.println("Invalid Marks");
+}else{
+if(i>=85&&i<=100) {
+	System.out.println("Grade A");
+}else if(i>=75&&i<=84){
+	System.out.println("Grade B");
+}else if(i>=35&&i<=74){
+	System.out.println("PASS");
+}
+else{ System.out.println("FAIL");}
+}
+}
+}
+
